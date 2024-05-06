ca1af4d8de43d3a9e56df337c5e9115601a1678b
Minh Le <minh.le@ledminh.dev>
2024-05-06 17:43:05 -0400

    CentralImage

--- src/app/(other)/projects/shop-landing-page-1/components/CentralImage/index.tsx
+++ src/app/(other)/projects/shop-landing-page-1/components/CentralImage/index.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+import centralImage from "../../assets/images/central-image.jpg";
+
 export default function CentralImage() {
   return (
-    <div className="bg-slate-600 text-white text-center p-2">
-      <p>Free shipping on all orders over $50</p>
-    </div>
+    <section className="relative">
+      <div className="absolute top-0 w-full h-full bg-gradient-to-r from-black/60 to-black/10" />
+      <div className="absolute top-0 w-full h-full bg-gradient-to-l from-black/60 to-black/10" />
+      <Image
+        src={centralImage}
+        alt="Central Image"
+        width="1680"
+        height="580"
+        className="w-full h-[34rem] object-cover"
+      />
+    </section>
   );
 }
