03fff7d21c6d2b96b586a8a820eb840a4d998e80
Minh Le <duyminh.le310@gmail.com>
2024-05-06 18:41:30 -0400

    CentralImage

---
+++ src/app/(other)/projects/shop-landing-page-1/components/CentralImage/data.ts
@@ -0,0 +1,14 @@
+import centralImage from "../../assets/images/central-image.jpg";
+
+const data = {
+  header: "Spring Collection",
+  paragraph:
+    "Time to jazz up your wardrobe with a mix of styles to make dressing up effortless and fun",
+  link: {
+    text: "Shop Now",
+    url: "/spring-collection",
+  },
+  image: centralImage,
+};
+
+export default data;
--- src/app/(other)/projects/shop-landing-page-1/components/CentralImage/index.tsx
+++ src/app/(other)/projects/shop-landing-page-1/components/CentralImage/index.tsx
@@ -1,29 +1,33 @@
 import Image from "next/image";
-import centralImage from "../../assets/images/central-image.jpg";
 import { RightArrowHeadIcon } from "../../assets/icons";
+import Link from "next/link";
+
+import data from "./data";
 
 export default function CentralImage() {
+  const { header, paragraph, link, image } = data;
+
   return (
     <section className="relative">
       <div className="absolute top-0 w-full h-full bg-gradient-to-r from-black/60 to-black/10" />
       <div className="absolute top-0 w-full h-full bg-gradient-to-l from-black/60 to-black/10" />
       <div className="absolute top-0 w-full h-full">
         <div className="flex flex-col justify-center items-center h-full text-white gap-2 p-2">
-          <h1 className="text-5xl font-bold">Spring Collection</h1>
-          <p className="text-center">
-            Time to jazz up your wardrobe with a mix of styles to make dressing
-            up effortless and fun
-          </p>
-          <button className="px-4 py-2 mt-4 bg-white text-black rounded-lg flex justify-between items-center gap-2">
-            <span>Shop Now</span>
+          <h1 className="text-5xl font-bold">{header}</h1>
+          <p className="text-center">{paragraph}</p>
+          <Link
+            href={link.url}
+            className="px-4 py-2 mt-4 bg-white text-black rounded-lg flex justify-between items-center gap-2"
+          >
+            <span>{link.text}</span>
             <div className="w-6 h-6">
               <RightArrowHeadIcon />
             </div>
-          </button>
+          </Link>
         </div>
       </div>
       <Image
-        src={centralImage}
+        src={image}
         alt="Central Image"
         width="1680"
         height="580"
