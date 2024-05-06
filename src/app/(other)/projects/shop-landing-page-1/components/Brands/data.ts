56f4af97348eb8635ecdc0f380a252dd1dc4390e
Minh Le <duyminh.le310@gmail.com>
2024-05-06 18:35:09 -0400

    Brands

---
+++ src/app/(other)/projects/shop-landing-page-1/components/Brands/data.ts
@@ -0,0 +1,43 @@
+import adidasImg from "../../assets/images/brands/adidas.png";
+import asosImg from "../../assets/images/brands/asos.png";
+import burberryImg from "../../assets/images/brands/burberry.png";
+import louisVuittonImg from "../../assets/images/brands/louis-vuitton.png";
+import ssenseImg from "../../assets/images/brands/ssense.png";
+
+const data = {
+  intro: "Shop By World Class Brands",
+  items: [
+    {
+      id: "0001",
+      name: "Adidas",
+      slug: "adidas",
+      image: adidasImg,
+    },
+    {
+      id: "0002",
+      name: "Asos",
+      slug: "asos",
+      image: asosImg,
+    },
+    {
+      id: "0003",
+      name: "Burberry",
+      slug: "burberry",
+      image: burberryImg,
+    },
+    {
+      id: "0004",
+      name: "Louis Vuitton",
+      slug: "louis-vuitton",
+      image: louisVuittonImg,
+    },
+    {
+      id: "0005",
+      name: "Ssense",
+      slug: "ssense",
+      image: ssenseImg,
+    },
+  ],
+};
+
+export default data;
--- src/app/(other)/projects/shop-landing-page-1/components/Brands/index.tsx
+++ src/app/(other)/projects/shop-landing-page-1/components/Brands/index.tsx
@@ -1,7 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import data from "./data";
+
 export default function Brands() {
+  const { intro, items } = data;
+
   return (
-    <div className="bg-slate-600 text-white text-center p-2">
-      <p>Free shipping on all orders over $50</p>
-    </div>
+    <section>
+      <h2 className="text-4xl font-bold text-center">{intro}</h2>
+      <ul className="flex flex-wrap justify-center items-center gap-4 mt-8">
+        {items.map((item) => (
+          <li key={item.id}>
+            <Link href={`/brands/${item.slug}`} className="w-40 block">
+              <Image src={item.image} alt={item.name} />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 }
