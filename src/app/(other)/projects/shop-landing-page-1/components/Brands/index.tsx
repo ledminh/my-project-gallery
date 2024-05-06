80e7e413bdb8cb6e858f3370c2113ecc6d4feb30
Minh Le <duyminh.le310@gmail.com>
2024-05-06 18:44:15 -0400

    alignment

--- src/app/(other)/projects/shop-landing-page-1/components/Brands/index.tsx
+++ src/app/(other)/projects/shop-landing-page-1/components/Brands/index.tsx
@@ -8,11 +8,11 @@ export default function Brands() {
 
   return (
     <section>
-      <h2 className="text-4xl font-bold text-center">{intro}</h2>
+      <h2 className="text-3xl md:text-4xl font-bold text-center">{intro}</h2>
       <ul className="flex flex-wrap justify-center items-center gap-4 mt-8">
         {items.map((item) => (
           <li key={item.id}>
-            <Link href={`/brands/${item.slug}`} className="w-40 block">
+            <Link href={`/brands/${item.slug}`} className="w-20 block md:w-40">
               <Image src={item.image} alt={item.name} />
             </Link>
           </li>
--- src/app/(other)/projects/shop-landing-page-1/components/CentralImage/index.tsx
+++ src/app/(other)/projects/shop-landing-page-1/components/CentralImage/index.tsx
@@ -12,9 +12,9 @@ export default function CentralImage() {
       <div className="absolute top-0 w-full h-full bg-gradient-to-r from-black/60 to-black/10" />
       <div className="absolute top-0 w-full h-full bg-gradient-to-l from-black/60 to-black/10" />
       <div className="absolute top-0 w-full h-full">
-        <div className="flex flex-col justify-center items-center h-full text-white gap-2 p-2">
+        <div className="flex flex-col justify-center items-center h-full text-white gap-2 p-2 text-center">
           <h1 className="text-5xl font-bold">{header}</h1>
-          <p className="text-center">{paragraph}</p>
+          <p>{paragraph}</p>
           <Link
             href={link.url}
             className="px-4 py-2 mt-4 bg-white text-black rounded-lg flex justify-between items-center gap-2"
