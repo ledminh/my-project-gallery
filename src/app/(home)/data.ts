7bd9b02ddb9ff00ad05af5dde0a26c263d0ece02
Minh Le <duyminh.le310@gmail.com>
2024-05-07 18:38:46 -0400

    Add image to the front page

---
+++ src/app/(home)/data.ts
@@ -0,0 +1,13 @@
+import LandingPage1Img from "./assets/images/landing-page-001.png";
+
+const data = [
+  {
+    id: "landing-page-1",
+    title: "Shop Landing Page 1",
+    link: "/projects/shop-landing-page-1",
+    description: "A simple landing page for a shop.",
+    image: LandingPage1Img,
+  },
+];
+
+export default data;
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
 
+import data from "./data";
+import Image from "next/image";
+
 export default function Home() {
   return (
     <>
@@ -9,14 +12,22 @@ export default function Home() {
         </h1>
       </header>
       <main>
-        <ul className="m-4 border border-slate-600 p-4 rounded-lg">
-          {projects.map((project) => (
-            <li key={project.slug}>
+        <ul className="m-4 flex justify-start gap-4">
+          {data.map((project) => (
+            <li key={project.id}>
               <Link
-                className="text-blue-500 hover:underline"
-                href={`/projects/${project.slug}`}
+                href={project.link}
+                className="shadow-md shadow-black rounded-md p-2 flex flex-col gap-2 justify-center items-center bg-slate-200 "
               >
-                {project.title}
+                <Image
+                  src={project.image}
+                  alt={project.title}
+                  width={400}
+                  height={250}
+                  className="w-60 h-60 rounded-md overflow-hidden"
+                />
+                <h2 className="font-bold text-lg">{project.title}</h2>
+                <p className="text-sm">{project.description}</p>
               </Link>
             </li>
           ))}
@@ -25,10 +36,3 @@ export default function Home() {
     </>
   );
 }
-
-const projects = [
-  {
-    slug: "shop-landing-page-1",
-    title: "Shop Landing Page 1",
-  },
-];
