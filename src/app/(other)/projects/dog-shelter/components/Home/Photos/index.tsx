dd8991e62b1b87806b5bf711918b49c8dda8668c
Minh Le <minh.le@ledminh.dev>
2024-05-10 17:42:08 -0400

    Dog Paws

--- src/app/(other)/projects/dog-shelter/components/Home/Photos/index.tsx
+++ src/app/(other)/projects/dog-shelter/components/Home/Photos/index.tsx
@@ -1,54 +1,10 @@
-import { DogPaws } from "../../../assets/icons";
-import Image from "next/image";
-import Dog1 from "../../../assets/images/dog1.png";
-import Dog2 from "../../../assets/images/dog2.png";
-import Dog3 from "../../../assets/images/dog3.png";
-import Dog4 from "../../../assets/images/dog4.png";
-
 export default function Photos() {
   return (
     <div className="flex justify-between items-center">
-      <div className="w-40 text-primary-1 -translate-x-5 translate-y-[7rem] relative">
-        <DogPaws xTop={0} yTop={0} xBottom={200} yBottom={265} />
-
-        <Image
-          src={Dog1}
-          alt="Dog 1"
-          width={200}
-          height={200}
-          className="absolute top-1/2 -translate-y-1/3 left-1/2 -translate-x-1/2 h-34 w-auto"
-        />
-      </div>
-      <div className="w-40 text-primary-1 relative">
-        <DogPaws xTop={320} yTop={0} xBottom={220} yBottom={342} />
-        <Image
-          src={Dog2}
-          alt="Dog 2"
-          width={200}
-          height={200}
-          className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 h-24 w-auto"
-        />
-      </div>
-      <div className="w-40 text-primary-1 relative">
-        <DogPaws xTop={580} yTop={0} xBottom={220} yBottom={343} />
-        <Image
-          src={Dog3}
-          alt="Dog 3"
-          width={200}
-          height={200}
-          className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 h-24 w-auto"
-        />
-      </div>
-      <div className="w-40 text-primary-1 translate-x-5 translate-y-[7rem] relative">
-        <DogPaws xTop={0} yTop={370} xBottom={200} yBottom={264} />
-        <Image
-          src={Dog4}
-          alt="Dog 4"
-          width={200}
-          height={200}
-          className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 h-24 w-auto"
-        />
-      </div>
+      <div className="paw-image-1 w-16 h-32 -translate-x-8 translate-y-24 sm:w-24 sm:h-40 md:w-36 md:h-52 md:translate-y-20 lg:translate-y-28 sm:-translate-x-10" />
+      <div className="paw-image-2 w-16 h-32 sm:w-24 sm:h-40 md:w-36 md:h-56" />
+      <div className="paw-image-3 w-16 h-32 sm:w-24 sm:h-40 md:w-36 md:h-56" />
+      <div className="paw-image-4 w-16 h-32 sm:w-24 sm:h-40 translate-y-16 md:w-36 md:h-56 md:translate-y-16 lg:translate-y-24" />
     </div>
   );
 }
